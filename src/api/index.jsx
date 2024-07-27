77ea192a696bd340c275668017bd3e0fb872f8a9
William Ongky Wijaya <william.ongkywow@gmail.com>
2024-07-27 15:12:36 +0700

    p

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://dompol.info";
+export const BASE_URL = "https://dompol.info";
 
 const useAxios = axios.create({
   baseURL: `${BASE_URL}/api`,
